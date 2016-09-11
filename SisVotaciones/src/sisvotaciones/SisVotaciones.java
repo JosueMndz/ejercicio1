f907751649d073482fd999f51da89ce75b435679
daniel <daniel@daniel-HP>
2016-09-11 12:15:01 -0600

    que tal?

--- SisVotaciones/src/sisvotaciones/SisVotaciones.java
+++ SisVotaciones/src/sisvotaciones/SisVotaciones.java
@@ -5,6 +5,8 @@
  */
 package sisvotaciones;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author daniel
@@ -16,6 +18,9 @@ public class SisVotaciones {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        JOptionPane.showMessageDialog(null, "Hola veve");
+        
     }
     
 }
